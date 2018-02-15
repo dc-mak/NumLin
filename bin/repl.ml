5ba42e41bebc5044cc49c8d748b9202728723f80
Dhruv Makwana <dc_mak@outlook.com>
2018-02-15 11:00:03 +0000

    Use incremental parsing

--- bin/repl.ml
+++ bin/repl.ml
@@ -14,32 +14,6 @@ open Base
 open Lt4la
 ;;
 
-let print_position () lexbuf =
-  let open Lexing in
-  let pos = lexbuf.lex_curr_p in
-  let cpos = pos.pos_cnum - pos.pos_bol + 1 in
-  let point =
-    let bytes = Bytes.create (cpos - 1) in
-    for i = 0 to cpos - 3 do
-      Bytes.set bytes i ' ';
-    done;
-    Bytes.set bytes (cpos-2) '^';
-    Bytes.to_string bytes in
-  point
-  (* Printf.sprintf "%s:%d:%d" pos.pos_fname *)
-  (* pos.pos_lnum (cpos)                     *)
-;;
-
-let parse_with_error lexbuf =
-  try Parser.prog Lexer.read lexbuf with
-  | Lexer.SyntaxError msg ->
-    Printf.sprintf "%a (%s)" print_position lexbuf msg
-    |> Or_error.error_string
-  | Parser.Error ->
-    Printf.sprintf "%a syntax error" print_position lexbuf
-    |> Or_error.error_string
-;;
-
 let string_of_exp exp =
   let buffer = Buffer.create 80 in
   Ast.pp_expression (Caml.Format.formatter_of_buffer buffer) exp;
@@ -65,21 +39,56 @@ let color col str =
   code ^ str ^ reset
 ;;
 
-let parse_and_print lexbuf =
-  let open Stdio.Out_channel in
-  match parse_with_error lexbuf with
-  | Ok value ->
-    string_of_exp value
-    |> String.split ~on:'\n'
-    |> String.concat ~sep:"\n  "
-    |> color B_Green
-    |> output_string stdout;
-  | Error err ->
-    Error.to_string_hum err
-    |> String.split ~on:'\n'
-    |> String.concat ~sep:"\n  "
+module Inc =
+  Parser.MenhirInterpreter
+;;
+
+let rec loop lexbuf = function
+  | Inc.InputNeeded _ as checkpoint ->
+    let token = Lexer.read lexbuf in
+    let startp, endp = lexbuf.lex_start_p, lexbuf.lex_curr_p in
+    let checkpoint = Inc.offer checkpoint (token, startp, endp) in
+    loop lexbuf checkpoint
+
+  | Inc.Shifting (_, _, _)
+  | Inc.AboutToReduce (_, _) as checkpoint ->
+    let checkpoint = Inc.resume checkpoint in
+    loop lexbuf checkpoint
+
+  | Inc.HandlingError env ->
+    let open Lexing in
+    let pos = lexbuf.lex_curr_p in
+    let cpos = pos.pos_cnum - pos.pos_bol in
+    let bytes = Bytes.create cpos in
+    for i = 0 to cpos - 2 do
+      Bytes.set bytes i ' ';
+    done;
+    Bytes.set bytes (cpos-1) '^';
+    Bytes.to_string bytes;
+    (* Printf.sprintf "%s:%d:%d" pos.pos_fname *)
+    (* pos.pos_lnum (cpos)                     *)
     |> color B_Red
-    |> output_string stdout;
+    |> Stdio.Out_channel.(output_string stdout)
+
+  | Inc.Accepted v ->
+    let open Stdio.Out_channel in
+    begin match v with
+    | Ok value ->
+      string_of_exp value
+      |> String.split ~on:'\n'
+      |> String.concat ~sep:"\n  "
+      |> color B_Green
+      |> output_string stdout;
+    | Error err ->
+      Error.to_string_hum err
+      |> String.split ~on:'\n'
+      |> String.concat ~sep:"\n  "
+      |> color B_Red
+      |> output_string stdout;
+    end
+
+  | Inc.Rejected ->
+    Stdio.Out_channel.(output_string stdout "Uh-oh")
 ;;
 
 (*
@@ -101,7 +110,7 @@ let loop = function
     let lexbuf = Lexing.from_string str in
     lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = "repl" };
     putstr (color B_Yellow "- ");
-    parse_and_print lexbuf;
+    loop lexbuf (Parser.Incremental.prog lexbuf.lex_curr_p);
     putstr (color B_Cyan ("\n> "));
     Stdio.Out_channel.(flush stdout)
 ;;
